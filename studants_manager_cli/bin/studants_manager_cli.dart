29022246df5022356e046b26757a25548f611442
Raphael Vasconcelos Nunes de Mello <Raphaelvnmello@hotmail.com>
2024-04-03 10:56:28 -0400

    initial structure

---
+++ studants_manager_cli/bin/studants_manager_cli.dart
@@ -0,0 +1,5 @@
+import 'package:studants_manager_cli/studants_manager_cli.dart' as studants_manager_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${studants_manager_cli.calculate()}!');
+}
---
+++ studants_manager_cli/test/studants_manager_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:studants_manager_cli/studants_manager_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
